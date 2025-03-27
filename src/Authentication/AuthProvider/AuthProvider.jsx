ff9b2974cdf355b1ac06d066670a42c710dc9502
hayder <hayderbd4290@gmail.com>
2025-03-27 23:15:45 +0600

    connect firebase and set env file

--- src/Authentication/AuthProvider/AuthProvider.jsx
+++ src/Authentication/AuthProvider/AuthProvider.jsx
@@ -1,16 +1,29 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react"
 import { createContext } from "react"
+import { auth } from "../FirebaseConfig/firebase.config";
 
 export const AuthContext = createContext(null)
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(false)
 
-    
+    const registerUser = (email, password)=>{
+        setLoading(true)
+       return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const loginUser = (email, password)=>{
+        setLoading(true)
+        signInWithEmailAndPassword(auth, email, password)
+    }
+
+
    
     const authInfo = {
         user,
-        loading
+        loading,
+        registerUser,
+        loginUser
     }
 
   return (
