ffeef10e36e781bfd952467f9f74e49731458c1b
hayder <hayderbd4290@gmail.com>
2025-04-02 00:57:46 +0600

    implement delete each product by id done

--- src/components/dashBoardComponents/page/AddProduct.jsx
+++ src/components/dashBoardComponents/page/AddProduct.jsx
@@ -1,18 +1,27 @@
 import DashBoardFrom from "../DashBoardFrom";
 
 const AddProduct = () => {
-  const categoryArr = ['Mobile Phones','Laptops & Tablets','Televisions',
-    'PCs & Accessories', 'Audio & Video', 'Gaming & Accessories', 'Home Appliances',
-    'Kitchen Appliances', 'Cameras', 'Consumables', 'Smart Homes', 'Accessories'
+  const categoryArr = [
+    "Mobile Phones",
+    "Laptops Tablets",
+    "Televisions",
+    "PCs Accessories",
+    "Audio Video",
+    "Gaming Accessories",
+    "Home Appliances",
+    "Kitchen Appliances",
+    "Cameras",
+    "Consumables",
+    "Smart Homes",
+    "Accessories",
   ];
-  const discountArr = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70]
+  const discountArr = [
+    0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70,
+  ];
 
   return (
     <div className="min-h-[90vh] flex justify-center items-center">
-      <DashBoardFrom
-       categoryArr={categoryArr}
-       discountArr={discountArr}
-      />
+      <DashBoardFrom categoryArr={categoryArr} discountArr={discountArr} />
     </div>
   );
 };
--- src/components/dashBoardComponents/page/AllProduct.jsx
+++ src/components/dashBoardComponents/page/AllProduct.jsx
@@ -1,18 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import useProduct from "../../../hooks/useProduct";
 import LoaderSipnner from "../../common/LoaderSipnner";
 import { FaEdit } from "react-icons/fa";
 import { RiDeleteBin2Fill } from "react-icons/ri";
+import Swal from "sweetalert2";
+import usePublicServer from "../../../hooks/usePublicServer";
 
 const AllProduct = () => {
-  const { productData, isLoading, refetch } = useProduct("");
-  
-  if(isLoading){
-    return <LoaderSipnner/>
+  const [category, setCategory] = useState("");
+  const { productData, isLoading, refetch } = useProduct(
+    category === "All" ? "" : category
+  );
+  const publicServer = usePublicServer();
+
+  const categoryArr = [
+    "All",
+    "Mobile Phones",
+    "Laptops Tablets",
+    "Televisions",
+    "PCs Accessories",
+    "Audio Video",
+    "Gaming Accessories",
+    "Home Appliances",
+    "Kitchen Appliances",
+    "Cameras",
+    "Consumables",
+    "Smart Homes",
+    "Accessories",
+  ];
+
+  if (isLoading) {
+    return <LoaderSipnner />;
   }
+
+  const handleDelete = async (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async(result) => {
+      if (result.isConfirmed) {
+       const {data} =  await publicServer.delete(`/deleteProduct/${id}`)
+       if(data.deletedCount>0){
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+        refetch();
+       }
+      }
+    });
+  };
 
   return (
     <div className="px-2 py-6">
+      {/* header */}
+      <div className="py-4 flex flex-wrap justify-between items-center">
+        <p className="text-xl font-semibold">Product: {productData?.length}</p>
+        <select
+          onChange={(e) => setCategory(e.target.value)}
+          className="select outline-none"
+        >
+          {categoryArr.map((item, index) => (
+            <option value={item} key={index}>
+              {item}
+            </option>
+          ))}
+        </select>
+      </div>
       <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
@@ -30,25 +90,34 @@ const AllProduct = () => {
           </thead>
           <tbody>
             {/* row 1 */}
-            {
-              productData.map((item, idx)=>(
-                <tr key={idx} className="bg-base-200">
+            {productData.map((item, idx) => (
+              <tr key={idx} className="bg-base-200">
                 <th>{idx + 1}</th>
-                <td><img src={item?.image} alt={item?.title} className="w-10 h-10 rounded-full" /></td>
+                <td>
+                  <img
+                    src={item?.image}
+                    alt={item?.title}
+                    className="w-10 h-10 rounded-full"
+                  />
+                </td>
                 <td>{item?.title}</td>
                 <td>{item?.category}</td>
                 <td>{item?.price} $</td>
                 <td>{item?.productCode}</td>
                 <td>{item?.stock}</td>
                 <td className="flex text-2xl items-center gap-4">
-                 <button className="text-blue-800 cursor-pointer"><FaEdit /></button>
-                 <button className="text-red-700 cursor-pointer"><RiDeleteBin2Fill /></button>
+                  <button className="text-blue-800 cursor-pointer">
+                    <FaEdit />
+                  </button>
+                  <button
+                    onClick={() => handleDelete(item?._id)}
+                    className="text-red-700 cursor-pointer"
+                  >
+                    <RiDeleteBin2Fill />
+                  </button>
                 </td>
               </tr>
-              ))
-            }
-            
-            
+            ))}
           </tbody>
         </table>
       </div>
