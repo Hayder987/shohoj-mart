61ce173d9dd8840110cd85f37fc944a3f189ed56
hayder <hayderbd4290@gmail.com>
2025-04-10 18:16:45 +0600

    offer and contact page created

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,10 +1,21 @@
 import React, { useEffect } from "react";
+import PageMargin from "../components/common/PageMargin";
+import offer1 from '../../src/assets/images/banner4.webp'
+import { Link } from "react-router";
 
 const Offers = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  return <div>Offers</div>;
+  return (
+  <div className="container mx-auto">
+    <PageMargin/>
+    <div className="p-6">
+      <Link to={'/collection'}><img src={offer1} alt="" className="hover:scale-105 duration-500" /></Link>
+    </div>
+    
+  </div>
+  );
 };
 
 export default Offers;
